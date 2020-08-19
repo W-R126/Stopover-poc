d7f3d5038acf8f90d5bb9c3454ded91085505344
rz-roy <rz.royzhang@gmail.com>
2020-08-19 11:17:58 +0200

    fix suggestions for the baggage view

--- src/Views/ExperimentalView/ExperimentalSearch/Components/SearchSection/Components/SearchInput/Components/BaggageView/index.tsx
+++ src/Views/ExperimentalView/ExperimentalSearch/Components/SearchSection/Components/SearchInput/Components/BaggageView/index.tsx
@@ -10,7 +10,10 @@ export default function BaggageView({ data }: BaggageViewProps): JSX.Element {
   return (
     <div className={css.ComponentContainer}>
       <img src={SuitCaseIcon} alt="Suitcase" />
-      {`${data.content.answer} ${data.content.weight}kg`}
+      <span className={css.Answer}>
+        {`${data.content.answer} ${data.content.weight}kg : `}
+      </span>
+      {`${data.title}?`}
     </div>
   );
 }
